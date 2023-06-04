0c2f9829fa2332c1b1bfc796be0c57788782332b
jb <theshitcoinert@gmail.com>
2023-06-05 00:00:19 +0200

    maj

--- src/sniper/uniswapV3Tests.ts
+++ src/sniper/uniswapV3Tests.ts
@@ -1,15 +1,13 @@
 import { FeeAmount, TICK_SPACINGS, computePoolAddress } from "@uniswap/v3-sdk";
 import { Token } from "@uniswap/sdk-core";
 import { ethers } from "ethers";
-import { GetTransaction } from "./class";
+import { GetTransaction } from "../library/class";
 import ABI from "../web3/abis/IERC20.json";
 import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json";
 
 async function fetchTokenData(address: string, wallet: GetTransaction) {
-    const provider = new ethers.JsonRpcProvider(
-        wallet.blockchainRouter.blockchain.connection
-    );
+    const provider = new ethers.JsonRpcProvider(wallet.blockchainRouter.blockchain.connection);
     const tokenContract = new ethers.Contract(address, ABI, provider);
     const decimals = await tokenContract.decimals();
     const symbol = await tokenContract.symbol();
@@ -25,11 +23,7 @@ async function fetchTokenData(address: string, wallet: GetTransaction) {
     return tokenData;
 }
 
-export async function getEth(
-    addr: string,
-    addr2: string,
-    wallet: GetTransaction
-) {
+export async function getEth(addr: string, addr2: string, wallet: GetTransaction) {
     const tokenData = await fetchTokenData(addr, wallet);
     const wethData = await fetchTokenData(addr2, wallet);
 
@@ -39,29 +33,21 @@ export async function getEth(
         wallet.getWallet()
     );
 
-    const readableAmount = ethers.parseUnits(
-        wallet.transaction.amount.toString(),
-        tokenData.decimals
-    );
+    const readableAmount = ethers.parseUnits(wallet.transaction.amount.toString(), tokenData.decimals);
 
-    const quotedAmountOut =
-        await quoterContract.quoteExactInputSingle.staticCall(
-            tokenData.address,
-            wethData.address,
-            FeeAmount.MEDIUM,
-            readableAmount,
-            0
-        );
+    const quotedAmountOut = await quoterContract.quoteExactInputSingle.staticCall(
+        tokenData.address,
+        wethData.address,
+        FeeAmount.MEDIUM,
+        readableAmount,
+        0
+    );
 
     const result = ethers.formatUnits(quotedAmountOut, tokenData.decimals);
     console.log(result);
 }
 
-export async function checkPool(
-    addr: string,
-    addr2: string,
-    wallet: GetTransaction
-) {
+export async function checkPool(addr: string, addr2: string, wallet: GetTransaction) {
     const tokenData = await fetchTokenData(addr, wallet);
     const wethData = await fetchTokenData(addr2, wallet);
     const fees = Object.keys(TICK_SPACINGS);
@@ -76,20 +62,12 @@ export async function checkPool(
                 tokenData.symbol,
                 tokenData.name
             ),
-            tokenB: new Token(
-                wethData.chainId,
-                wethData.address,
-                wethData.decimals,
-                wethData.symbol,
-                wethData.name
-            ),
+            tokenB: new Token(wethData.chainId, wethData.address, wethData.decimals, wethData.symbol, wethData.name),
             fee: fees[i] as unknown as FeeAmount,
             // fee: FeeAmount.MEDIUM,
         });
 
-        const code = await wallet
-            .getWallet()
-            ?.provider?.getCode(currentPoolAddress);
+        const code = await wallet.getWallet()?.provider?.getCode(currentPoolAddress);
         if (code && code !== "0x") {
             console.log("adresse: ", currentPoolAddress, "fees: ", fees[i]);
         }
